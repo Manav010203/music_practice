88141c7e29dce4bc7717537a77d0493333d2b732
Manav010203 <manav01022003@gmail.com>
2024-12-12 19:33:03 +0530

    optimization

--- app/api/stream/downvote/route.ts
+++ app/api/stream/downvote/route.ts
@@ -1,50 +1,105 @@
 
+// import prisma from "@/app/lib/db";
+// import { getServerSession } from "next-auth";
+// import { NextRequest, NextResponse } from "next/server";
+// import {z} from "zod"
+
+// const UpvoteSchema = z.object({
+//      streamId : z.string(),
+
+// })
+
+// export async function POST (req: NextRequest){
+//     const session = await getServerSession();
+   
+//     const user =await prisma.user.findFirst({
+//         where:{
+//             email : session?.user?.email ?? ""
+//         }
+//     })
+//     if(!user){
+//         return NextResponse.json({
+//             message: "Unauthenticated"
+//         },{
+//             status:403
+//         })
+//     }
+
+//     try{
+//         const data = UpvoteSchema.parse(await req.json)
+//         await prisma.upvote.delete({
+//             where:{
+//                 userId_streamId:{
+//                     userId:user.id,
+//                     streamId: data.streamId
+//                 }
+//             }
+//         })
+//         return NextResponse.json({
+//             message:"Done!"
+//         })
+//     }
+//     catch(e){
+//         console.error(e)
+//         return NextResponse.json({
+//             message:"ERROR while downvoting"
+//         },{
+//             status:403
+//         })
+//     }
+
+// }
+
+
 import prisma from "@/app/lib/db";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-import {z} from "zod"
+import { z } from "zod";
 
-const UpvotSchema = z.object({
-     streamId : z.string(),
+const UpvoteSchema = z.object({
+  streamId: z.string()
+});
 
-})
+export async function POST(req: NextRequest) {
+  const session = await getServerSession();
 
-export async function POST (req: NextRequest){
-    const session = await getServerSession();
-   
-    const user =await prisma.user.findFirst({
-        where:{
-            email : session?.user?.email ?? ""
+  const user = await prisma.user.findFirst({
+    where:{
+      email: session?.user?.email ??""
+    }
+  })
+  if (!user) {
+    return NextResponse.json(
+      {
+        message: "Unauthenticated",
+      },
+      {
+        status: 403,
+      },
+    );
+  }
+
+  try {
+    const data = UpvoteSchema.parse(await req.json());
+    await prisma.upvote.delete({
+      where: {
+        userId_streamId:{
+            userId: user.id,
+            streamId: data.streamId,
         }
-    })
-    if(!user){
-        return NextResponse.json({
-            message: "Unauthenticated"
-        },{
-            status:403
-        })
-    }
-
-    try{
-        const data = UpvotSchema.parse(await req.json)
-        await prisma.upvote.delete({
-            where:{
-                userId_streamId:{
-                    userId:user.id,
-                    streamId: data.streamId
-                }
-            }
-        })
-        return NextResponse.json({
-            message:"Done!"
-        })
-    }
-    catch(e){
-        return NextResponse.json({
-            message:"ERROR while upvoting"
-        },{
-            status:403
-        })
-    }
-
+      },
+    });
+    return NextResponse.json({
+      message: "Done!",
+    });
+  } catch (e) {
+    return NextResponse.json(
+      {
+        message: "Error while upvoting",
+      },
+      {
+        status: 403,
+      },
+    );
+  }
 }
